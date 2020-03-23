53330229cd8297f0fb0952ac70d708c8af1bfee5
Kholkin <kholkins@mail.ru>
2020-03-23 19:56:13 +0300

    Add

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -6,11 +6,16 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     ImageView playPause;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
         playPause = (ImageView)findViewById(R.id.imageViewIconPlay);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.original);
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
     }
 
     public void previous(View view) {
